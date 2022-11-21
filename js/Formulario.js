c659a27522ceb8f640623ea300115f48e8d8a9fd
Ezequiel Garay <ezequielgaray37@gmail.com>
2022-11-21 12:19:41 -0300

    Anexo de formulario

--- js/Formulario.js
+++ js/Formulario.js
@@ -1,3 +1,43 @@
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const telefono = document.getElementById("teléfono")
+const mensaje = document.getElementById("mensaje")
+const boton = document.getElementById("BotonDatos")
+
+
+boton.onclick = () => {
+    const Usuario = {
+        Nombre: nombre.value,
+        Correo: correo.value,
+        Telefono: telefono.value,
+        Mensaje: mensaje.value
+    }
+    Swal.fire({
+        title: "Bienvenido",
+        text: "Hola $(nombre.value). ¿Desea enviar este mensaje?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Su menjase no ha sido enviado.',
+        
+          )
+        }
+      })
+     localStorage.setItem("InfoUsuario", JSON.stringify(Usuario))
+}
+
+
+
+
+
+
+
+
 let inputs = document.getElementsByClassName ("Formulario_Input");
 for (let i = 0; i < inputs.length; i++){
     inputs[i].addEventListener ( "keyup", function () {
@@ -9,3 +49,5 @@ if (this.value.length>=1) {
     });
     
 }
+
+
